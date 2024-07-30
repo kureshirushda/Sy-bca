467ddbc41a2db7c6f7e1dba6f2a1197e87612148
kureshirushda <kureshimoin255@gmail.com>
2024-07-30 21:18:06 +0530

    Update calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+[30/07, 9:04 PM] Alhumdulila: #include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
+[30/07, 9:15 PM] Alhumdulila: Calculator
